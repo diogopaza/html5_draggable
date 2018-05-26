0448d8a988d92167d1ca4f83ef1177bfb2bff447
Diogo Paza <diogopazacvel@gmail.com>
2018-05-26 11:03:40 -0300

    draggable html5

---
+++ script.js
@@ -0,0 +1,11 @@
+/* drag and drop events
+	drag, dragstart, dragenter, dragexit, dragleave, dragover, 
+*/
+
+var dragItem = document.getElementById("dragElement");
+var dropLoc = document.getElementById("dropLocation");
+
+
+console.log(dropLocation);
+
+console.log(dragItem);
